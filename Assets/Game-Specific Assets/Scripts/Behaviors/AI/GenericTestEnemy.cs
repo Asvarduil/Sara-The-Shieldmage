0c190eb713968a48a4f917b851b69c635f9e36e4
Asvarduil <andrew.russel.gray@gmail.com>
2014-10-19 22:14:20 -0500

    19 October livestream additions - improved Sara runcycle frames.  Created Flash Fade behavior on the Summoned Block.  Added SFX for the Flash Fading.  Created a basic enemy with a non-functional AI.

---
+++ Assets/Game-Specific Assets/Scripts/Behaviors/AI/GenericTestEnemy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GenericTestEnemy : DebuggableBehavior
+{
+	#region Enumerations
+
+	public enum AIState
+	{
+		Idle,
+		Active
+	}
+
+	#endregion Enumerations
+
+	#region Variables / Properties
+
+	public AIState State;
+
+	private Dictionary<Func<bool>, Action> States;
+
+	#endregion Variables / Properties
+
+	#region Hooks
+
+	public void Start()
+	{
+		States = new Dictionary<Func<bool>, Action>();
+		States.Add(CanSeePlayer, LoafAbout);
+	}
+
+	public void Update()
+	{
+		foreach(KeyValuePair<Func<bool>, Action> state in States)
+		{
+			if(! state.Key())
+				continue;
+
+			state.Value();
+			break;
+		}
+	}
+
+	#endregion Hooks
+
+	#region Methods
+
+	#endregion Methods
+
+	#region Conditions
+
+	private bool CanSeePlayer()
+	{
+		return false;
+	}
+
+	#endregion Conditions
+
+	#region Mannerisms
+
+	private void LoafAbout()
+	{
+		// Snap into a Slim Tim! (TM)
+	}
+
+	#endregion Mannerisms
+}
--- Assets/Game-Specific Assets/Scripts/Behaviors/Control/FlashingDecayEffect.cs
+++ Assets/Game-Specific Assets/Scripts/Behaviors/Control/FlashingDecayEffect.cs
@@ -1,19 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DecayingEffect : MonoBehaviour 
+public class FlashingDecayEffect : DebuggableBehavior 
 {
 	#region Variables / Properties
 
-	public float EffectDuration = 5.0f;
 	public string ShaderTintFieldName = "_TintColor";
-	public float MaterialFadeRate = 0.05f;
-	public float MinimumFadeAmount = 0.1f;
+	public int NumberOfFlashes = 3;
+	public float FlashInterval = 1.0f;
+	public float InitialAlpha = 1.0f;
+	public float FadeRate = 0.25f;
+	public AudioClip FlashSoundEffect;
 	public GameObject ExpirationEffect;
 
+	private int _remainingFlashes;
+	private float _nextFlash;
+	private float _currentAlphaStrength;
 	private float _spawnTime;
 	private float _expireTime;
 	private Material _objectMaterial;
+
+	private Maestro _maestro;
+
+	private float EffectDuration
+	{
+		get { return FlashInterval * NumberOfFlashes; }
+	}
 
 	#endregion Variables / Properties
 
@@ -21,15 +33,19 @@ public class DecayingEffect : MonoBehaviour
 
 	public void Start()
 	{
+		_maestro = Maestro.Instance;
+
 		_spawnTime = Time.time;
 		_expireTime = _spawnTime + EffectDuration;
-
+		_remainingFlashes = NumberOfFlashes;
+		_nextFlash = _spawnTime + FlashInterval;
+		
 		_objectMaterial = renderer.materials[0];
 	}
 
 	public void Update()
 	{
-		FadeObjectAlpha();
+		FlashFade();
 		CheckExpiration();
 	}
 
@@ -37,10 +53,23 @@ public class DecayingEffect : MonoBehaviour
 
 	#region Methods
 
-	public void FadeObjectAlpha()
+	private void FlashFade()
 	{
 		Color target = renderer.material.GetColor(ShaderTintFieldName);
-		target.a = (target.a - ((MaterialFadeRate * target.a) / EffectDuration));
+
+		if(Time.time >= _nextFlash)
+		{
+			if(FlashSoundEffect != null)
+				_maestro.PlayOneShot(FlashSoundEffect);
+
+			_nextFlash = Time.time + FlashInterval;
+			_remainingFlashes--;
+			target.a = InitialAlpha;
+		}
+		else
+		{
+			target.a = (target.a - ((FadeRate * target.a) / EffectDuration));
+		}
 
 		renderer.material.SetColor(ShaderTintFieldName, target);
 	}
@@ -49,7 +78,7 @@ public class DecayingEffect : MonoBehaviour
 	{
 		if(Time.time <= _expireTime)
 			return;
-
+		
 		if(ExpirationEffect != null)
 			GameObject.Instantiate(ExpirationEffect, transform.position, Quaternion.identity);
 		
