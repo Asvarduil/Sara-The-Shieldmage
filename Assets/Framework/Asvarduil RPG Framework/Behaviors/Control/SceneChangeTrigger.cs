daf7337656e76e4c9a7030ab4b1f1df882bf10a6
Andrew Gray <andrew.russel.gray@gmail.com>
2015-01-08 13:45:47 -0600

    Added Castle Town to the game.  Created Scene Change Triggers and the associated scripts.  Got a scene link between the World Map and Crystal Cave 1 established both ways.

---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/Control/SceneChangeTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeTrigger : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    public string AllowedTag = "Player";
+    public SceneState TargetState;
+
+    private TransitionManager _transition;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public void Start()
+    {
+        _transition = TransitionManager.Instance;
+    }
+
+    public void OnTriggerEnter(Collider who)
+    {
+        if (who.tag != AllowedTag)
+            return;
+
+        _transition.PrepareSceneChange(TargetState);
+        _transition.ChangeScenes();
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
