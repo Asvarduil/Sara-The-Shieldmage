ef4c0f2296ef508c217489931637f7748c6c85ad
Andrew R. Gray <andrew.russel.gray@gmail.com>
2015-01-11 15:47:17 -0600

    Fixed formatting on TransitionManager.

--- Assets/Framework/Asvarduil RPG Framework/Behaviors/Control/TransitionManager.cs
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/Control/TransitionManager.cs
@@ -6,28 +6,28 @@ public class TransitionManager : ManagerBase<TransitionManager>
 {
 	#region Variables / Properties
 
-    public float FadeRate = 0.05f;
+	public float FadeRate = 0.05f;
 	public SceneState OriginalState;
 	public SceneState TargetState;
 
-    private Fader _fader;
-    private bool _transitionStarted = false;
-    private string _targetSceneName;
+	private Fader _fader;
+	private bool _transitionStarted = false;
+	private string _targetSceneName;
 
 	#endregion Variables / Properties
 
-    #region Hooks
+	#region Hooks
 
-    public void Start()
-    {
-        _fader = FindObjectOfType<Fader>();
-    }
+	public void Start()
+	{
+    		_fader = FindObjectOfType<Fader>();
+	}
 
-    #endregion Hooks
+	#endregion Hooks
 
-    #region Methods
+	#region Methods
 
-    public void PrepareSceneChange(SceneState state, bool isSource = false)
+	public void PrepareSceneChange(SceneState state, bool isSource = false)
 	{
 		if(isSource)
 			OriginalState = state;
@@ -37,33 +37,34 @@ public class TransitionManager : ManagerBase<TransitionManager>
 
 	public void ChangeScenes(bool toSource = false)
 	{
-        _transitionStarted = true;
+		_transitionStarted = true;
 
 		if(toSource)
 		{
 			DebugMessage("Transitioning back to the source scene...");
-            TargetState = OriginalState;
-            _targetSceneName = OriginalState.SceneName;
+			TargetState = OriginalState;
+			_targetSceneName = OriginalState.SceneName;
 		}
 		else
 		{
 			DebugMessage("Transitioning to the target scene...");
-            _targetSceneName = TargetState.SceneName;
+			_targetSceneName = TargetState.SceneName;
 		}
 
-        //StartCoroutine(SceneChangeProcess());
-        Application.LoadLevel(_targetSceneName);
+        	//StartCoroutine(SceneChangeProcess());
+        	Application.LoadLevel(_targetSceneName);
 	}
 
-    private IEnumerator SceneChangeProcess()
-    {
-        _fader.FadeOut(FadeRate);
+	private IEnumerator SceneChangeProcess()
+	{
+		_fader.FadeOut(FadeRate);
 
-        while (!_fader.ScreenHidden)
-            yield return 0;
-
-        Application.LoadLevel(_targetSceneName);
-    }
+		while (!_fader.ScreenHidden)
+			yield return 0;
+			
+		Application.LoadLevel(_targetSceneName);
+		
+	}
 
 	#endregion Methods
 }
