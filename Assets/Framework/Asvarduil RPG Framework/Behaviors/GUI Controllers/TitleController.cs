8a9d97f412a32ccaa42f45cd1b61da1f1c8b2f0a
Andrew Gray <andrew.russel.gray@gmail.com>
2015-02-11 20:52:54 -0600

    Added 2F to Nuuria Castle.  Added various doodads to Nuuria Castle.

---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/GUI Controllers/TitleController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : ManagerBase<TitleController>
+{
+    #region Variables / Properties
+
+    private TitlePresenter _title;
+    //private SettingsPresenter _settings;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public void Start()
+    {
+        _title = GetComponentInChildren<TitlePresenter>();
+    }
+
+    public void PresentTitleScreen()
+    {
+        _title.PresentElements();
+    }
+
+    public void PresentContinueScreen()
+    {
+
+    }
+
+    public void PresentSettingsScreen()
+    {
+
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/GUI Presenters/TitlePresenter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitlePresenter : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    //public Image TitlePane;
+    //public Button NewGameButton;
+    //public Button ContinueButton;
+    //public Button SettingsButton;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public void PresentElements()
+    {
+
+    }
+
+    #endregion Hooks
+}
