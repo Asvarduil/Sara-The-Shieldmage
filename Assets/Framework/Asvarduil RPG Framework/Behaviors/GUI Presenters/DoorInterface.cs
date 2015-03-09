6eadf2f0728f754c923267e2b25fd4977c2285a8
Andrew Gray <andrew.russel.gray@gmail.com>
2015-03-08 21:33:56 -0500

    Got Shop map object in game; created previous scene trigger script; created Door Interface behavior.  Need to refactor the Door/Entity interface to talk to a generic event controller that is not the Dialogue Manager.

---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/GUI Presenters/DoorInterface.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorInterface : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    public string AffectedTag = "Player";
+    public string InteractText = "Talk";
+
+    public SceneState TargetState;
+
+    private TransitionManager _transition;
+    private DialogueController _controller;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public virtual void Start()
+    {
+        _transition = TransitionManager.Instance;
+        _controller = DialogueController.Instance;
+    }
+
+    public virtual void OnInteraction()
+    {
+        _transition.PrepareSceneChange(TargetState);
+        _transition.ChangeScenes();
+    }
+
+    public void OnTriggerEnter(Collider who)
+    {
+        if (who.tag != AffectedTag)
+            return;
+
+        _controller.PrepareInteraction(InteractText, () => OnInteraction());
+    }
+
+    public void OnTriggerExit(Collider who)
+    {
+        if (who.tag != AffectedTag)
+            return;
+
+        _controller.ClearInteraction();
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/Game Systems/PreviousSceneTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreviousSceneTrigger : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    public string AllowedTag = "Player";
+
+    private Fader _fader;
+    private Maestro _maestro;
+    private TransitionManager _transition;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public void Start()
+    {
+        _transition = TransitionManager.Instance;
+    }
+
+    public void OnTriggerEnter(Collider who)
+    {
+        if (who.tag != AllowedTag)
+            return;
+
+        _transition.ChangeScenes(true);
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
