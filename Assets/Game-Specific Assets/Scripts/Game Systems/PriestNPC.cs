a433d7f49a0fdf600fcdbc50bf0b83fa603bb25f
Andrew Gray <andrew.russel.gray@gmail.com>
2015-04-10 21:08:02 -0500

    Created Priest NPC script, and the NPC dialogue for the Nuuria City Priestess.  Need to fill out the Nuuria City Church locale with other doodads.

---
+++ Assets/Game-Specific Assets/Scripts/Game Systems/PriestNPC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class PriestNPC : EntityTextInterface
+{
+    #region Variables / Properties
+
+    public string deadPartyMembersThread = "Revive Party";
+    public string genericChatterThread = "Chatter";
+
+    private PartyManager _party;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public override void Start()
+    {
+        base.Start();
+        _party = PartyManager.Instance;
+    }
+
+    public override void OnInteraction()
+    {
+        string threadName = _party.FindAvailableCharacters().Any(p => p.Health.IsDead)
+            ? deadPartyMembersThread
+            : genericChatterThread;
+
+        TextThread thread = _entityText.GetThreadByName(threadName);
+        if (thread == default(TextThread))
+            throw new Exception("Could not find a thread on NPC " + gameObject.name + " named " + threadName);
+
+        _controller.PresentTextThread(thread);
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
