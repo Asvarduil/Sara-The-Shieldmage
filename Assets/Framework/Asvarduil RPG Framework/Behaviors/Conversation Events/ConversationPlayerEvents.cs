81f159e4d9b28fa7cfe609bf8a1debf520c02a54
Andrew Gray <andrew.russel.gray@gmail.com>
2015-04-16 18:32:13 -0500

    Created Floor Generator and test scene, to help make building this game slightly less boring, and slightly quicker.

---
+++ Assets/Framework/Asvarduil RPG Framework/Behaviors/Conversation Events/ConversationPlayerEvents.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ConversationPlayerEvents : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    private PartyManager _party;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    public void Start()
+    {
+        _party = PartyManager.Instance;
+    }
+
+    public void FullyHealParty(List<string> args)
+    {
+        List<PlayableCharacter> partyMembers = _party.FindAvailableCharacters().ToList();
+
+        for (int i = 0; i < partyMembers.Count; i++)
+        {
+            PlayableCharacter current = partyMembers[i];
+            current.Health.Heal(current.Health.MaxHP);
+        }
+    }
+
+    public void ReviveDeadPlayers(List<string> args)
+    {
+        List<PlayableCharacter> partyMembers = _party.FindAvailableCharacters().ToList();
+
+        for(int i = 0; i < partyMembers.Count; i++)
+        {
+            PlayableCharacter current = partyMembers[i];
+            if (current.Health.IsDead)
+                current.Health.Heal(current.Health.MaxHP);
+        }
+
+        // TODO: Visual Effect!
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    #endregion Methods
+}
---
+++ Assets/Framework/Asvarduil RPG Framework/Middleware/Floor Generator/FloorGenerator.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGenerator : DebuggableBehavior
+{
+    #region Variables / Properties
+
+    public GameObject FloorTile;
+    public Vector2 FloorDimensions;
+
+    private Vector3 _offset;
+
+    #endregion Variables / Properties
+
+    #region Hooks
+
+    [ContextMenu("Delete Floor")]
+    public void DestroyFloor()
+    {
+        int children = transform.childCount;
+        for(int i = 0; i < children; i++)
+        {
+            Transform current = transform.GetChild(i);
+            GameObject.DestroyImmediate(current.gameObject);
+        }
+    }
+
+    [ContextMenu("Generate Floor")]
+    public void GenerateFloor()
+    {
+        DetermineFloorOffset();
+
+        Transform thisTransform = gameObject.transform;
+        for(int xOffset = 0; xOffset < FloorDimensions.x; xOffset++)
+        {
+            for(int yOffset = 0; yOffset < FloorDimensions.y; yOffset++)
+            {
+                Vector3 currentOffset = new Vector3(xOffset, 0, yOffset);
+                Vector3 position = transform.position + _offset + currentOffset;
+                GameObject currentTile = GameObject.Instantiate(FloorTile, position, Quaternion.identity) as GameObject;
+                currentTile.transform.SetParent(thisTransform);
+                currentTile.name = currentTile.name.Substring(0, currentTile.name.Length - 7);
+            }
+        }
+    }
+
+    #endregion Hooks
+
+    #region Methods
+
+    private void DetermineFloorOffset()
+    {
+        _offset = Vector3.zero;
+
+        _offset.x -= (FloorDimensions.x % 2 > 0)
+            ? (FloorDimensions.x / 2) - 1
+            : FloorDimensions.x / 2;
+        _offset.z -= (FloorDimensions.y % 2 > 0)
+            ? (FloorDimensions.y / 2) - 1
+            : FloorDimensions.y / 2;
+    }
+
+    #endregion Methods
+}
